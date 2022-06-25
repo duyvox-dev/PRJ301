580474e7969bc571bc3dd0e23ba4ea3522f4c161
Võ Văn Bảo Duy <vobaoduy123@gmail.com>
2022-06-25 09:51:13 +0700

    first setup coding project
    first setup coding project

---
+++ LaptopsGo/src/main/java/com/fptuni/prj301/assignment/model/User/UserDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fptuni.prj301.assignment.model.User;
+
+/**
+ *
+ * @author vobao
+ */
+public class UserDAO {
+    
+}
