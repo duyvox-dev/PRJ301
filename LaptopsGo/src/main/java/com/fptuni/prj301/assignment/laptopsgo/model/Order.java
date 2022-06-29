1f563c3def72892d14c43c73cf60d8c7e81ad00e
tiepem <empvtse150335@fpt.edu.vn>
2022-06-29 07:04:30 +0700

    add model
    add model

--- LaptopsGo/src/main/java/com/fptuni/prj301/assignment/laptopsgo/model/Order.java
+++ LaptopsGo/src/main/java/com/fptuni/prj301/assignment/laptopsgo/model/Order.java
@@ -12,5 +12,54 @@ package com.fptuni.prj301.assignment.laptopsgo.model;
  * @author voduy
  */
 public class Order {
+    int ProductID;
+    int quantity;
+    int sellerID;
+    int buyerID;
+
+    public Order() {
+    }
+
+    
+    
+    public Order(int ProductID, int quantity, int sellerID, int buyerID) {
+        this.ProductID = ProductID;
+        this.quantity = quantity;
+        this.sellerID = sellerID;
+        this.buyerID = buyerID;
+    }
+
+    public int getProductID() {
+        return ProductID;
+    }
+
+    public void setProductID(int ProductID) {
+        this.ProductID = ProductID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(int sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public int getBuyerID() {
+        return buyerID;
+    }
+
+    public void setBuyerID(int buyerID) {
+        this.buyerID = buyerID;
+    }
+    
     
 }
--- LaptopsGo/src/main/java/com/fptuni/prj301/assignment/laptopsgo/model/User.java
+++ LaptopsGo/src/main/java/com/fptuni/prj301/assignment/laptopsgo/model/User.java
@@ -12,6 +12,85 @@ package com.fptuni.prj301.assignment.laptopsgo.model;
  * @author voduy
  */
 public class User {
+   int id;
+   String username;
+   String password;
+   String email;
+   String fullname;
+   String role;
+   Integer banStatus;
+
+    public User(int id, String username, String password, String email, String fullname, String role, Integer banStatus) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.role = role;
+        this.banStatus = banStatus;
+    }
+
+    public User() {
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getBanStatus() {
+        return banStatus;
+    }
+
+    public void setBanStatus(Integer banStatus) {
+        this.banStatus = banStatus;
+    }
+   
    
     
 }
